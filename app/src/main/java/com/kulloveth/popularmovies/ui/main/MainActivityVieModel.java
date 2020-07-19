e7a7183de65a3a9534a18a56355fae13079aad0d
kulloveth <kulloveth@gmail.com>
2020-07-19 13:07:47 +0100

    hide api key

--- app/src/main/java/com/kulloveth/popularmovies/ui/main/MainActivityVieModel.java
+++ app/src/main/java/com/kulloveth/popularmovies/ui/main/MainActivityVieModel.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
 import com.kulloveth.popularmovies.ApiUtils;
 import com.kulloveth.popularmovies.ProgressListener;
